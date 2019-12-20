c8bab39ddedbfe1910e6e5482de5af576f953504
Andy Lu <yilunglu.andy@gmail.com>
2019-12-19 18:40:47 -0800

    Added leaderboard, submit score

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,16 +1,44 @@
 const prev_guess_box = document.getElementById("prev_guess_box");
+const pause_overlay = document.getElementById("pause_overlay");
+const gameover_overlay = document.getElementById("gameover_overlay");
+const score_list = document.getElementById("score_list");
+const score_name = document.getElementById("score_name");
+const submit_btn = document.getElementById("submit");
+const scores_btn = document.getElementById("scores_btn");
+const final_score = document.getElementById("final_score");
 const word_box = document.getElementById("word_box");
 const combo_num = document.getElementById("combo_num");
+const begin_btn = document.getElementById("begin_btn");
+const instructions = document.getElementById("instructions");
+const timer = document.getElementById("timer_num");
+const countdown = document.getElementById("countdown");
+let score_num = document.getElementById("score_num");
 
+let gameStart = false;
+let paused = false;
+let time = {
+  minute: 10,
+  second: 0
+};
 let previous_guesses = [];
 let guessWord = null;
 let placeholder = [];
 let comboMultiplier = 0;
+let currentScore = 0;
 
 combo_num.textContent = `${comboMultiplier}x`;
 
+const herokuGetWord = "https://guessthewordgame.herokuapp.com/random";
+const herokuGetScores = "https://guessthewordgame.herokuapp.com/highscores";
+const herokuSubmitScore = "https://guessthewordgame.herokuapp.com/submitscore";
+
+begin_btn.addEventListener("click", e => {
+  instructions.classList.add("hideInstructions");
+  getWord();
+});
+
 const getWord = () => {
-  fetch("https://guessthewordgame.herokuapp.com/random").then(res =>
+  fetch(herokuGetWord).then(res =>
     res
       .text()
       .then(word => {
@@ -27,8 +55,6 @@ const getWord = () => {
   );
 };
 
-getWord();
-
 const setupGuessAlphabet = () => {
   prev_guess_box.innerHTML = "";
   for (let i = 65; i <= 90; i++) {
@@ -41,41 +67,51 @@ const setupGuessAlphabet = () => {
   }
 };
 
-const keyevent = event => {
-  if (event.keyCode >= 65) {
-    if (event.keyCode <= 90) {
-      let key = String.fromCharCode(event.keyCode).toLowerCase();
-      let lastCorrectLetter = "";
-      if (!previous_guesses.includes(key)) {
-        previous_guesses.push(key);
-        for (let i = 0; i < guessWord.length; i++) {
-          if (guessWord[i] == key) {
-            placeholder[i] = key.toUpperCase();
-            lastCorrectLetter = key.toUpperCase();
-          }
-        }
-        if (!guessWord.includes(key)) {
-          document.getElementById(`letter_${key}`).classList.add("wrong");
-          let guesses_num = document.getElementById("guesses_num");
-          let wrongs = guesses_num.textContent;
-          guesses_num.textContent = parseInt(wrongs) + 1;
-          if (comboMultiplier >= 0) {
-            comboMultiplier = 0;
-          }
-          previous_guesses.push(key);
-        } else {
-          previous_guesses.push(key);
-          if (comboMultiplier < 4) {
-            comboMultiplier += 1;
-          }
-          document.getElementById(`letter_${key}`).classList.add("right");
-          let score_num = document.getElementById("score_num");
-          let score = score_num.textContent;
-          score_num.textContent = parseInt(score) + 10 * comboMultiplier;
-        }
+const setupUI = () => {
+  const bottom_section = document.getElementById("bottom_section");
+  bottom_section.classList.add("visible");
+};
 
-        updatePlaceholder(lastCorrectLetter);
-        combo_num.textContent = `${comboMultiplier}x`;
+const keyevent = event => {
+  if (event.keyCode === 32) {
+    console.log("spacebar");
+    paused = !paused;
+    pause_overlay.classList.toggle("visible");
+  } else if (event.keyCode >= 65) {
+    if (event.keyCode <= 90) {
+      if (!paused) {
+        let key = String.fromCharCode(event.keyCode).toLowerCase();
+        let lastCorrectLetter = "";
+        if (!previous_guesses.includes(key)) {
+          if (!guessWord.includes(key)) {
+            document.getElementById(`letter_${key}`).classList.add("wrong");
+            let guesses_num = document.getElementById("guesses_num");
+            let wrongs = guesses_num.textContent;
+            guesses_num.textContent = parseInt(wrongs) + 1;
+            if (comboMultiplier >= 0) {
+              comboMultiplier = 0;
+            }
+          } else {
+            if (comboMultiplier < 4) {
+              comboMultiplier += 1;
+            }
+            document.getElementById(`letter_${key}`).classList.add("right");
+          }
+          previous_guesses.push(key);
+          for (let i = 0; i < guessWord.length; i++) {
+            if (guessWord[i] == key) {
+              placeholder[i] = key.toUpperCase();
+              lastCorrectLetter = key.toUpperCase();
+              console.log(guessWord[i], key);
+              let score = score_num.textContent;
+              console.log(parseInt(score) + 10 * comboMultiplier);
+              currentScore = parseInt(score) + 10 * comboMultiplier;
+              score_num.textContent = currentScore;
+            }
+          }
+          updatePlaceholder(lastCorrectLetter);
+          combo_num.textContent = `${comboMultiplier}x`;
+        }
       }
     }
   }
@@ -116,8 +152,146 @@ const checkWin = () => {
   }
 };
 
+const setTimer = () => {
+  const timerInterval = setInterval(() => {
+    if (!paused) {
+      if (time.second === 0) {
+        time.minute -= 1;
+        time.second = 59;
+      } else if (time.second <= 59) {
+        time.second -= 1;
+      }
+      if (time.minute === 0) {
+        if (time.second === 0) {
+          time.second = 0;
+          clearInterval(timerInterval);
+          gameover();
+        }
+      }
+      timer.textContent = `${time.minute}:${
+        time.second <= 9 ? `0${time.second}` : time.second
+      }`;
+    }
+  }, 1000);
+};
+
+const setCountdown = () => {
+  let count = 2;
+  const createCountMessage = text => {
+    countdown.innerHTML = "";
+    let p = document.createElement("p");
+    let countText = document.createTextNode(text);
+    p.appendChild(countText);
+    p.classList.add("revealed");
+    countdown.appendChild(p);
+  };
+  const countdownTimer = setInterval(() => {
+    countdown.classList.toggle("revealed");
+    switch (count) {
+      case 2:
+        createCountMessage("READY");
+        count -= 1;
+        break;
+      case 1:
+        createCountMessage("SET");
+        count -= 1;
+        break;
+      case 0:
+        createCountMessage("GO!");
+        beginGame();
+        gameStart = true;
+        clearInterval(countdownTimer);
+        break;
+      default:
+        break;
+    }
+  }, 1000);
+};
+
+const beginGame = () => {
+  setTimeout(() => {
+    word_box.classList.add("visible");
+    document.addEventListener("keyup", keyevent);
+    setTimer();
+    countdown.innerHTML = "";
+  }, 1000);
+};
+
+const gameover = () => {
+  document.removeEventListener("keyup", keyevent);
+  gameover_overlay.classList.add("visible");
+  final_score.textContent = `Your score: ${currentScore}`;
+};
+
+const getScores = () => {
+  fetch(herokuGetScores)
+    .then(res => res.json())
+    .then(data => {
+      data.map((score, idx) => {
+        let div = document.createElement("div");
+        div.classList.add("score_row");
+        let name_p = document.createElement("p");
+        name_p.classList.add("score_name");
+        let score_p = document.createElement("p");
+        score_p.classList.add("score_score");
+        let userName = document.createTextNode(`${idx + 1}. ${score.name}`);
+        let userScore = document.createTextNode(`Score: ${score.score}`);
+        name_p.appendChild(userName);
+        score_p.appendChild(userScore);
+        div.appendChild(name_p);
+        div.appendChild(score_p);
+        score_list.appendChild(div);
+      });
+    });
+};
+
+submit_btn.addEventListener("click", e => {
+  e.preventDefault();
+  console.log("submit");
+  console.log(score_name.value);
+  if (score_name.value.trim() === "") {
+    document.getElementById("submit_message").textContent = "Enter a name!";
+  } else if (score_name.value.trim().length < 2) {
+    document.getElementById("submit_message").textContent =
+      "Minimum 2 letter name!";
+  } else {
+    fetch(herokuSubmitScore, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: score_name.value.trim(),
+        score: currentScore
+      })
+    }).then(res => {
+      if (res.status === 200) {
+        document.getElementById("submit_message").textContent =
+          "Score submitted!";
+        document
+          .getElementById("submit_score")
+          .classList.add("hideInstructions");
+      } else {
+        document.getElementById("submit_message").textContent =
+          "Oh no! Something went wrong...";
+      }
+    });
+  }
+});
+
+scores_btn.addEventListener("click", () => {
+  document.getElementById("submit_score").classList.add("hideInstructions");
+  document.getElementById("highscore").classList.add("visible");
+  getScores();
+});
+
 const init = () => {
+  setupUI();
   setupGuessAlphabet();
-  document.addEventListener("keyup", keyevent);
   updatePlaceholder();
+  if (!gameStart) {
+    setCountdown();
+  } else {
+    document.addEventListener("keyup", keyevent);
+  }
 };
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const fs = require("fs");
-const readline = require("readline");
 const path = require("path");
+const { Client } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 let words = null;
 
+const client = new Client({
+  connectionString:
+    "postgres://ekrqrlxyjmvdqy:21e7b8fb252f8fad4935b27586dabca449c29edcf7c35ec21682aff1b1bac0f1@ec2-174-129-234-111.compute-1.amazonaws.com:5432/dblck480b993d3",
+  ssl: true
+});
+
+client.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to DB!");
+  }
+});
+
 fs.readFile("./newwords.txt", (err, contents) => {
   words = contents.toString().split("\n");
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/client"));
 app.get("/", (req, res) => {
@@ -18,6 +36,30 @@ app.get("/", (req, res) => {
 
 app.get("/random", (req, res) => {
   res.send(words[Math.floor(Math.random() * words.length - 1)]);
+});
+
+app.post("/submitscore", (req, res) => {
+  let name = req.body.name;
+  let score = req.body.score;
+  client.query(
+    "INSERT INTO highscore (name, score) VALUES ($1, $2)",
+    [name, score],
+    (err, result) => {
+      if (result) {
+        res.sendStatus(200);
+      }
+    }
+  );
+});
+
+app.get("/highscores", (req, res) => {
+  client.query(
+    "SELECT * FROM highscore ORDER BY score desc",
+    [],
+    (err, result) => {
+      res.send(result.rows);
+    }
+  );
 });
 
 app.listen(PORT, () => {
